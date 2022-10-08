349a0dc77a10d8b1b241bd203d952ec7ff9586b2
Nihad Valiyev <nihadev@code.edu.az>
2022-10-09 00:46:55 +0400

    add files to repo

---
+++ homework-09.10.2022/homework-09.10.2022/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace homework_09._10._2022
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ////task1
+            //string a = "Hello";
+            //string b = "World";
+            //Console.WriteLine(string.Concat(a, b));
+
+            ////task2
+            //string a = "HelloWorld";
+            //string b = "World";
+            //Console.WriteLine(a.Contains(b));
+
+            ////task3
+            //string a = "Hello";
+            //string b = string.Copy(a);
+            //Console.WriteLine(b);
+
+            ////task4
+            //string a = "Hello";
+            //string b = a.Insert(2, “_World_”);
+            //Console.WriteLine(b);
+
+            ////task5
+            //string a = "Hello ";
+            //string b = a.Trim();
+            //Console.WriteLine(b);
+        }
+    }
+}
